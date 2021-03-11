522f5a210d20280532b358df1cc5d5a9302c1e68
Cheng Che <chengche6230@gmail.com>
2021-03-11 22:22:28 +0800

    lab 2-1

--- lab2/src/lab2-1.py
+++ lab2/src/lab2-1.py
@@ -1,9 +1,8 @@
 import numpy as np
 import cv2
 
-
 def histogram(img):
-    inten = np.zeros(256, np.uint8)
+    inten = np.zeros(256, np.uint32)
     for i in range(len(img)):
         for j in range(len(img[i])):
             inten[img[i,j,0]] += 1
@@ -16,26 +15,24 @@ def histogram(img):
         accSum[i] = accSum[i]/total
     return accSum
 
-def enhance(img, accSUm, SCALE):
+def enhance(img, accSum, SCALE):
     for i in range(256):
-        accSum[i] = int(accSum[i] * SCALE)
+        accSum[i] = np.uint32(accSum[i] * SCALE)
     tmp_img = img.copy()
     for i in range(len(tmp_img)):
         for j in range(len(tmp_img[i])):
             tmp_img[i,j] = accSum[img[i,j,0]]
-            #print(img[i,j])
-            #print(accSum[img[i,j,0]])
-            #print(tmp_img[i,j])
-            #input()
     return tmp_img
 
-SCALE = 255
-img = cv2.imread('../img/mj.tif')
-
-accSum = histogram(img)
-img_enhan = enhance(img, accSum, SCALE)
-
-cv2.imshow('before', img)
-cv2.imshow('after', img_enhan)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':    
+    
+    SCALE = 255
+    img = cv2.imread('../img/mj.tif')
+    
+    accSum = histogram(img)
+    img_enhan = enhance(img, accSum, SCALE)
+    
+    cv2.imshow('before', img)
+    cv2.imshow('after', img_enhan)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
