58978beb7232d5dbf7409cac139692e5714efcbf
Samuel Yu <samuelyuyy@gmail.com>
2021-03-18 22:24:56 +0800

    Fix comments and stuctures

--- lab3/src/lab3-1.py
+++ lab3/src/lab3-1.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2
 
+threshold = 180
 filename = '../video/vtest.avi'
 
 cap = cv2.VideoCapture(filename)
-frame_idx = 0
 backSub = cv2.createBackgroundSubtractorMOG2()
 shadowval = backSub.getShadowValue()
 
@@ -20,8 +20,6 @@ while cap.isOpened():
     
     # Connected Component
     # TODO: Seed Filling Algorithm
-    if frame_idx == 0:
-        print(nmask.shape)
     cc = np.zeros(nmask.shape)
 
     map_dict = {}
@@ -51,7 +49,7 @@ while cap.isOpened():
             j += 1
         i += 1
 
-    my_dict = {'rectangles': {}, 'stats': {}}
+    my_dict = {}
     i = 0
     for row in cc:
         j = 0
@@ -60,41 +58,40 @@ while cap.isOpened():
                 if pixel in map_dict:
                     cc[i][j] = map_dict[pixel]
 
-                if pixel not in my_dict['stats']:
-                    my_dict['stats'][ pixel ] = 0
-                    my_dict['rectangles'][pixel] = [[j, i], [j, i]]
+                if pixel not in my_dict:
+                    my_dict[pixel] = {
+                        'stats': 0,
+                        'rectangles': [[j, i], [j, i]]
+                    }
                 else:
-                    rec_1_x = my_dict['rectangles'][pixel][0][0]
-                    rec_1_y = my_dict['rectangles'][pixel][0][1]
-                    rec_2_x = my_dict['rectangles'][pixel][1][0]
-                    rec_2_y = my_dict['rectangles'][pixel][1][1]
+                    rec_1_x = my_dict[pixel]['rectangles'][0][0]
+                    rec_1_y = my_dict[pixel]['rectangles'][0][1]
+                    rec_2_x = my_dict[pixel]['rectangles'][1][0]
+                    rec_2_y = my_dict[pixel]['rectangles'][1][1]
 
                     if j < rec_1_x:
-                        my_dict['rectangles'][pixel][0][0] = j
+                        my_dict[pixel]['rectangles'][0][0] = j
                     if i < rec_1_y:
-                        my_dict['rectangles'][pixel][0][1] = i
+                        my_dict[pixel]['rectangles'][0][1] = i
                     if j > rec_2_x:
-                        my_dict['rectangles'][pixel][1][0] = j
+                        my_dict[pixel]['rectangles'][1][0] = j
                     if i > rec_2_y:
-                        my_dict['rectangles'][pixel][1][1] = i
+                        my_dict[pixel]['rectangles'][1][1] = i
                 
-                my_dict['stats'][ pixel ] += 1
-                
+                my_dict[pixel]['stats'] += 1
             j += 1
         i += 1
 
     # Rectangle
-    for label in my_dict['rectangles']:
-        if label in my_dict['stats'] and my_dict['stats'][label] > 180:
-            ptr1 = my_dict['rectangles'][label][0]
-            ptr2 = my_dict['rectangles'][label][1]
+    for label in my_dict:
+        if my_dict[label]['stats'] > threshold:
+            ptr1 = my_dict[label]['rectangles'][0]
+            ptr2 = my_dict[label]['rectangles'][1]
             cv2.rectangle(frame, (ptr1[0], ptr1[1]), (ptr2[0], ptr2[1]), (0, 255, 0), 2)
 
 
     # Display the frame
     cv2.imshow("frame", frame)
     cv2.waitKey(33)
-
-    frame_idx += 1
 
 cv2.destroyAllWindows()
