178521c6528264b37aa2fca5b3a47cb1cfd1b406
Cheng Che <chengche6230@gmail.com>
2021-05-04 00:15:09 +0800

    HOG done

--- Lab8/src/Lab8.py
+++ Lab8/src/Lab8.py
@@ -1,2 +1,33 @@
 import cv2
 import numpy as np
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # 0 for camera, second arg for solving warning
+
+filename = '../../lab3/video/vtest.avi'
+#cap = cv2.VideoCapture(filename)
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+while(True):
+    
+    ret, frame = cap.read()
+    
+    rects, weights = hog.detectMultiScale(frame, winStride=(4,4), scale=1.1, useMeanshiftGrouping=False)
+    try:
+        for rect in rects:
+            cv2.rectangle(frame, (rect[0], rect[1]), 
+                                 (rect[0] + rect[2], rect[1] + rect[3]), 
+                                 (0, 255, 0), 2)
+    except:
+        print("Not detected.")
+        pass
+    
+    cv2.imshow('frame', frame)
+    
+    key = cv2.waitKey(33)
+    if key == ord('q') or key == 27: # Esc
+        break
+
+cap.release()
+cv2.destroyAllWindows()
