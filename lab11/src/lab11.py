909568c70a8d765925aadb50298cc7f5f9feb7a9
Samuel Yu <samuelyuyy@gmail.com>
2021-05-30 22:45:03 +0800

    Modify code to allow using three detectors

--- lab11/src/lab11.py
+++ lab11/src/lab11.py
@@ -10,15 +10,17 @@ detector_SURF = cv2.xfeatures2d.SURF_create()
 detector_SIFT = cv2.xfeatures2d.SIFT_create()
 detector_ORB = cv2.ORB_create()
 
-
 img_set = img_sets[0]
+dector = detector_ORB
+
+
 img1 = cv2.imread(f'../img/{img_set}/1.png')
 img2 = cv2.imread(f'../img/{img_set}/2.png')
 
-kp1, des1 = detector_ORB.detectAndCompute(img1, None)
-kp2, des2 = detector_ORB.detectAndCompute(img2, None)
+kp1, des1 = dector.detectAndCompute(img1, None)
+kp2, des2 = dector.detectAndCompute(img2, None)
 
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
 matches = bf.match(des1, des2)
 matches = sorted(matches, key=lambda x: x.distance)
 
