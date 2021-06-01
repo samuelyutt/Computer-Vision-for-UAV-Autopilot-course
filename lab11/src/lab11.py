0e8cc21f4db6aa4afadd49246f13655751af8a37
Samuel Yu <samuelyuyy@gmail.com>
2021-06-01 22:00:03 +0800

    Add speed testing codes

--- lab11/src/lab11.py
+++ lab11/src/lab11.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 
 img_sets = [
     'eiffel_tower',
@@ -12,7 +13,7 @@ detector_ORB = cv2.ORB_create()
 
 # Select which img_set and detector to use here
 img_set = img_sets[0]
-detector = detector_ORB
+detector = detector_SURF
 
 
 img1 = cv2.imread(f'../img/{img_set}/1.png')
@@ -24,6 +25,19 @@ kp2, des2 = detector.detectAndCompute(img2, None)
 bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
 matches = bf.match(des1, des2)
 matches = sorted(matches, key=lambda x: x.distance)
+
+# For speed testing
+# start_time = time.time()
+# for _ in range(100):
+#     kp1, des1 = detector.detectAndCompute(img1, None)
+#     kp2, des2 = detector.detectAndCompute(img2, None)
+
+#     bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+#     matches = bf.match(des1, des2)
+#     matches = sorted(matches, key=lambda x: x.distance)
+# end_time = time.time()
+# print(end_time - start_time)
+########
 
 matching_result = cv2.drawMatches(img1, kp1, img2, kp2, matches[:30], None)
 
